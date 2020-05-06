5278b970c0b117196eafce7d9d56214115a34d12
luoyusoft <luoyusoft@126.com>
2020-05-06 15:47:09 +0800

    1、新增webmagic模块 2、mybatisplus模块连接池替换成Hikari

---
+++ webmagic-demo/src/main/java/com/jinhaoxun/webmagicdemo/WebmagicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jinhaoxun.webmagicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmagicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebmagicDemoApplication.class, args);
+    }
+
+}
