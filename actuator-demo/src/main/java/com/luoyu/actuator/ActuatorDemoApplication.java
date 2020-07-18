94159bea5c56bee5b4e61b1d7480f83ddd800fa1
luoyusoft <luoyusoft@126.com>
2020-07-18 08:35:18 +0800

    1、优化代码

---
+++ actuator-demo/src/main/java/com/luoyu/actuator/ActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luoyu.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorDemoApplication.class, args);
+    }
+
+}
